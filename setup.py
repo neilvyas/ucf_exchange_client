5c88d1f17ec644a299ff8e4f7fd6dca876b65963
Neil Vyas <vyas.neil.nv@gmail.com>
2016-11-14 16:01:16 -0600

    adding package metadata

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+  name = 'ucf_exchange_client',
+  packages = ['ucf_exchange_client'],
+  version = '0.1',
+  description = 'A client wrapper library for UCF Exchange algo trading server.',
+  author = 'Texas UCF',
+  author_email = 'texasucfmgmt@gmail.com',
+  url = 'https://github.com/neilvyas/ucf_exchange_client',
+  download_url = 'https://github.com/neilvyas/ucf_exchange_client/tarball/0.1',
+)
